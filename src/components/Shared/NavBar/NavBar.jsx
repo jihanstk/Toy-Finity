5b71c8896d800785643b2547d0bd34e20c09d26d
jihanstkm <jihanstk@gmail.com>
2023-05-18 10:44:14 +0600

    added navbar

--- src/components/Shared/NavBar/NavBar.jsx
+++ src/components/Shared/NavBar/NavBar.jsx
@@ -1,35 +1,87 @@
+import { useState } from "react";
+import { IoMdClose, IoIosMenu, IoMdSearch } from "react-icons/io";
+import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 const NavBar = () => {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "All Toys", link: "/all-toys" },
+    { name: "My Toys", link: "/my-toys" },
+    { name: "Add a Toy", link: "/add-toy" },
+    { name: "Blog", link: "/blog" },
+  ];
+  let [open, setOpen] = useState(false);
   return (
-    <div className="navbar bg-base-100 sticky top-0">
-      <div className="flex-1">
-        <a className="uppercase font-bold text-xl">TOYFINITY</a>
-      </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
+    <div className="shadow-md w-full fixed top-0 left-0 z-10 ">
+      <div className="flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div
+          className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+    text-gray-800"
+        >
+          <span className="text-3xl text-indigo-600 mr-1 pt-2"></span>
+          TOYFINITY
+        </div>
+
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-20 " : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <NavLink
+                to={`${link.link}`}
+                className={({ isActive }) =>
+                  isActive ? "text-orange-700" : ""
+                }
+              >
+                {link.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+
+        <div
+          className={`form-control md:pb-0 pb-12 absolute  z-[-1] right-0 w-1/2 transition-all  duration-500 ease-in ${
+            showSearchBar ? "top-24 right-6" : "top-[-490px]"
+          }`}
+        >
           <input
-            type="text"
-            placeholder="Search"
             className="input input-bordered"
+            type="text"
+            name="search"
+            placeholder="search"
           />
         </div>
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-            </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+        <div className="flex items-center gap-6">
+          <div
+            onClick={() => setOpen(!open)}
+            className="text-2xl cursor-pointer md:hidden"
           >
-            <li></li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
+            {open ? <IoMdClose></IoMdClose> : <IoIosMenu></IoIosMenu>}
+          </div>
+          <div
+            onClick={() => setShowSearchBar(!showSearchBar)}
+            className="text-2xl  cursor-pointer "
+          >
+            <IoMdSearch></IoMdSearch>
+          </div>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
