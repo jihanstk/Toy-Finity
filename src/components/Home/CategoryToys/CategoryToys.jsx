8dca8b27a9f10f86ea16af652b024863bc037507
jihanstkm <jihanstk@gmail.com>
2023-05-20 01:12:12 +0600

    add category by toy

--- src/components/Home/CategoryToys/CategoryToys.jsx
+++ src/components/Home/CategoryToys/CategoryToys.jsx
@@ -1,116 +1,209 @@
+import { useEffect, useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
 
 const CategoryToys = () => {
+  const [tabText, setTabText] = useState("sport");
+  const [categoryData, setCategoryData] = useState([]);
+
+  const handleTab = (name) => {
+    setTabText("");
+    setTabText(name);
+  };
+  useEffect(() => {
+    fetch(`http://localhost:5021/category-data?category=${tabText}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCategoryData(data);
+        console.log(data);
+      });
+  }, [tabText]);
   return (
     <div className="my-24 text-center">
       <Tabs>
         <TabList>
-          <Tab>Rock</Tab>
-          <Tab>Ambulance</Tab>
-          <Tab>Sweet</Tab>
+          <Tab onClick={() => handleTab("sport")}>Sport</Tab>
+          <Tab onClick={() => handleTab("ambulance")}>Ambulance</Tab>
+          <Tab onClick={() => handleTab("sweet")}>Sweet</Tab>
+          <Tab onClick={() => handleTab("old")}>Old</Tab>
         </TabList>
         <div className="text-center">
           <TabPanel>
-            <div className="md:flex gap-5">
-              <div className="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                  <img
-                    src="https://images.pexels.com/photos/92615/pexels-photo-92615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Buy Now</button>
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-5">
+              {categoryData.map((data) => {
+                return (
+                  <div
+                    key={data._id}
+                    className="card w-96 bg-base-100 shadow-xl"
+                  >
+                    <figure className="h-[300px]">
+                      <img src={data.photo} alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{data.toyName}</h2>
+                      <p className="text-left">Price : {data.price}</p>
+                      <div className="flex items-center gap-2 my-3">
+                        <ReactStars
+                          classNames="text-slate-500 inline"
+                          count={5}
+                          value={data.rating}
+                          edit={false}
+                          size={14}
+                          isHalf={true}
+                          emptyIcon={<i className="far fa-star"></i>}
+                          halfIcon={<i className="fa fa-star-half-alt"></i>}
+                          fullIcon={<i className="fa fa-star"></i>}
+                          activeColor="text-slate-500"
+                        />
+                        <span className="text-orange-700 font-bold ">
+                          {data.rating}
+                        </span>
+                      </div>
+                      <div className="card-actions justify-center">
+                        <Link to={`/details-toy/${data._id}`}>
+                          <button className="btn btn-error">
+                            View Details
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                  <img
-                    src="https://images.pexels.com/photos/92615/pexels-photo-92615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
           </TabPanel>
           <TabPanel>
-            <div className="md:flex gap-5">
-              <div className="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                  <img
-                    src="https://images.pexels.com/photos/92615/pexels-photo-92615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Buy Now</button>
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-5">
+              {categoryData.map((data) => {
+                return (
+                  <div
+                    key={data._id}
+                    className="card w-96 bg-base-100 shadow-xl"
+                  >
+                    <figure className="h-[300px]">
+                      <img src={data.photo} alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{data.toyName}</h2>
+                      <p className="text-left">Price : {data.price}</p>
+                      <div className="flex items-center gap-2 my-3">
+                        <ReactStars
+                          classNames="text-slate-500 inline"
+                          count={5}
+                          value={data.rating}
+                          edit={false}
+                          size={14}
+                          isHalf={true}
+                          emptyIcon={<i className="far fa-star"></i>}
+                          halfIcon={<i className="fa fa-star-half-alt"></i>}
+                          fullIcon={<i className="fa fa-star"></i>}
+                          activeColor="text-slate-500"
+                        />
+                        <span className="text-orange-700 font-bold ">
+                          {data.rating}
+                        </span>
+                      </div>
+                      <div className="card-actions justify-center">
+                        <Link to={`/details-toy/${data._id}`}>
+                          <button className="btn btn-error">
+                            View Details
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                  <img
-                    src="https://images.pexels.com/photos/92615/pexels-photo-92615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
           </TabPanel>
           <TabPanel>
-            <div className="md:flex gap-5">
-              <div className="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                  <img
-                    src="https://images.pexels.com/photos/92615/pexels-photo-92615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Buy Now</button>
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-5">
+              {categoryData.map((data) => {
+                return (
+                  <div
+                    key={data._id}
+                    className="card w-96 bg-base-100 shadow-xl"
+                  >
+                    <figure className="h-[300px]">
+                      <img src={data.photo} alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{data.toyName}</h2>
+                      <p className="text-left">Price : {data.price}</p>
+                      <div className="flex items-center gap-2 my-3">
+                        <ReactStars
+                          classNames="text-slate-500 inline"
+                          count={5}
+                          value={data.rating}
+                          edit={false}
+                          size={14}
+                          isHalf={true}
+                          emptyIcon={<i className="far fa-star"></i>}
+                          halfIcon={<i className="fa fa-star-half-alt"></i>}
+                          fullIcon={<i className="fa fa-star"></i>}
+                          activeColor="text-slate-500"
+                        />
+                        <span className="text-orange-700 font-bold ">
+                          {data.rating}
+                        </span>
+                      </div>
+                      <div className="card-actions justify-center">
+                        <Link to={`/details-toy/${data._id}`}>
+                          <button className="btn btn-error">
+                            View Details
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                  <img
-                    src="https://images.pexels.com/photos/92615/pexels-photo-92615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Buy Now</button>
+                );
+              })}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-5">
+              {categoryData.map((data) => {
+                return (
+                  <div
+                    key={data._id}
+                    className="card w-96 bg-base-100 shadow-xl"
+                  >
+                    <figure className="h-[300px]">
+                      <img src={data.photo} alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{data.toyName}</h2>
+                      <p className="text-left">Price : {data.price}</p>
+                      <div className="flex items-center gap-2 my-3">
+                        <ReactStars
+                          classNames="text-slate-500 inline"
+                          count={5}
+                          value={data.rating}
+                          edit={false}
+                          size={14}
+                          isHalf={true}
+                          emptyIcon={<i className="far fa-star"></i>}
+                          halfIcon={<i className="fa fa-star-half-alt"></i>}
+                          fullIcon={<i className="fa fa-star"></i>}
+                          activeColor="text-slate-500"
+                        />
+                        <span className="text-orange-700 font-bold ">
+                          {data.rating}
+                        </span>
+                      </div>
+                      <div className="card-actions justify-center">
+                        <Link to={`/details-toy/${data._id}`}>
+                          <button className="btn btn-error">
+                            View Details
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
           </TabPanel>
         </div>
--- src/components/Home/ChildrenVideo/ChildrenVideo.jsx
+++ src/components/Home/ChildrenVideo/ChildrenVideo.jsx
@@ -1,10 +1,48 @@
+import { IoIosPlayCircle } from "react-icons/io";
+
 const ChildrenVideo = () => {
   return (
-    <div>
+    <div className="my-24 relative">
       <img
+        className="rounded-xl"
         src="https://cdn.shopify.com/s/files/1/1152/4590/files/video_1903x_68d6b0d8-8a04-4667-a075-a616bab789ef.jpg?v=1676955196"
         alt=""
       />
+      <div className="absolute top-1/4 left-0 w-full ">
+        <div className="md:w-1/2 mx-auto">
+          <h1 className="md:text-5xl   text-center font-bold text-white mx-auto">
+            We make your children happier with the best toys
+          </h1>
+          <div className="w-1/4 mx-auto">
+            {/* <button className="md:text-7xl text-4xl text-orange-700 btn btn-ghost">
+              <IoIosPlayCircle></IoIosPlayCircle>
+            </button> */}
+            {/* The button to open modal */}
+            <label htmlFor="my-modal-4" className="">
+              <IoIosPlayCircle className="md:text-7xl text-4xl text-orange-700 cursor-pointer mx-auto mt-4"></IoIosPlayCircle>
+            </label>
+
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+            <label
+              htmlFor="my-modal-4"
+              className="modal flex-none w-full  cursor-pointer"
+            >
+              <label className=" relative " htmlFor="">
+                <iframe
+                  className="rounded-xl youtube "
+                  width="800"
+                  height="500"
+                  src="https://www.youtube.com/embed/MIpQKMlZY1g"
+                  title="kids toy"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
+              </label>
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
